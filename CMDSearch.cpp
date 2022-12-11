f9ad2b6c81e20e396a212188ea6febac9a4c5e10
Emanuel Tin Rukavina <rukavinaet@gmail.com>
2022-12-11 22:59:12 +0100

    recognition-system-works

--- CMDSearch.cpp
+++ CMDSearch.cpp
@@ -3,22 +3,32 @@
 
 using namespace std;
 
-int main() {
-string site;
-cout << "Enter Desired site: " << endl;
-cin >> site;
-string baseUrl;
-if(site=="Facebook"){
-	baseUrl="https://www.facebook.com/";
-	cout << baseUrl << endl;
-    system("pause");
-}
-else if(site=="Youtube"){
-	baseUrl="https://www.youtube.com/user/";
-	cout << baseUrl << endl;
-}
+int main()
+{
+    string site;
+    cout << "Enter Desired site: " << endl;
+    cin >> site;
+    string baseUrl;
+    string first_two = site.substr(0, 2);
+    char c = ':';
+    baseUrl = "https://www.facebook.com/";
 
-//system(std::string("start " + baseUrl).c_str());
-	return 0;
-    system("pause");
+    cout << baseUrl << endl << first_two << endl;
+    if (site.find(c) != std::string::npos)
+    {
+        std::cout << "Character found" << std::endl;
+        system("pause");
+
+        // system(std::string("start " + baseUrl).c_str());
+        return 0;
+    }
+    else
+    {
+
+
+        // system(std::string("start " + baseUrl).c_str());
+        system("pause");
+                return 0;
+
+    }
 }
