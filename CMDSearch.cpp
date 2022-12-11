1068364f22f312cd8fbfecbef0beb0b8eb5a8db1
Emanuel Tin Rukavina <rukavinaet@gmail.com>
2022-12-11 23:02:03 +0100

    comments

--- CMDSearch.cpp
+++ CMDSearch.cpp
@@ -5,15 +5,23 @@ using namespace std;
 
 int main()
 {
+    //uzmi straincu
     string site;
     cout << "Enter Desired site: " << endl;
     cin >> site;
+    //pripremi string za link
     string baseUrl;
+    //uzmi prva dva slova i stavi ih u strinh
     string first_two = site.substr(0, 2);
+    //mogući znakovi
     char c = ':';
+
+    //base url će ovisiti o commani
     baseUrl = "https://www.facebook.com/";
 
     cout << baseUrl << endl << first_two << endl;
+
+    //provjeri dali upis ima onaj preodređeni znak
     if (site.find(c) != std::string::npos)
     {
         std::cout << "Character found" << std::endl;
