89f18d9f0b7ec35929e93b96c1e7b57de4647a52
Emanuel Tin Rukavina <rukavinaet@gmail.com>
2022-12-12 20:50:55 +0100

    verison 0.1

--- CMDSearch.cpp
+++ CMDSearch.cpp
@@ -2,26 +2,40 @@
 #include <cstring>
 using namespace std;
 
-//globalne stvari
-void myFunction();
+// globalne stvari
+void GoWeb();
+void help();
 std::string baseUrl;
+std::string ProjectWeb;
 
 int main()
 {
-    //očisti sustav i pripremi "site"- unos korisnika
+    // očisti sustav i pripremi "site"- unos korisnika
     system("cls");
     std::string site;
 
-    std::cout << "## CommandSearch 0.1 ##\nMore information is avaliable at https://rukavinaet.github.io/CommandSearch\n\n ";
+    std::cout << "## CommandSearch 0.1 ##\nMore information is avaliable at https://rukavinaet.github.io/CommandSearch\nFor help type 'help'\n\n ";
     std::getline(std::cin, site);
-    if (site == "options")
+    if (site == "")
     {
-        std::cout << "These are options and they are coming soon.\nReturning back...\n";
+        system("cls");
         main();
     }
     else
     {
-        //sve razmake zamijeni sa crticom
+        if (site == "options")
+        {
+            std::cout << "These are options and they are coming soon.\nReturning back...\n";
+            main();
+        }
+        else if (site == "help")
+        {
+            system("cls");
+            help();
+        }
+        else{
+             
+        // sve razmake zamijeni sa crticom
         for (int i = 0; i < site.length(); ++i)
         {
             if (site[i] == ' ')
@@ -31,32 +45,54 @@ int main()
         }
 
         char c = ':';
-        //ovo će se koristit kada bude multiple statements
-        char d= '+';
-        //ako ima dvotočku onda radi ovo
+        // ovo će se koristit kada bude multiple statements
+        char d = '+';
+        // ako ima dvotočku onda radi ovo
         if (site.find(c) != std::string::npos)
         {
             string first_two = site.substr(0, 2);
             site.erase(0, 3);
-            if (first_two == "GH"){baseUrl = "https://github.com/search?q=" + site;}
-            else if (first_two == "LD"){baseUrl = "https://www.linkedin.com/search/results/all/?keywords=" + site;}
-            else if (first_two == "SO"){baseUrl = "https://stackoverflow.com/search?q=" + site;}
+            if (first_two == "GH")
+            {
+                baseUrl = "https://github.com/search?q=" + site;
+            }
+            else if (first_two == "LD")
+            {
+                baseUrl = "https://www.linkedin.com/search/results/all/?keywords=" + site;
+            }
+            else if (first_two == "SO")
+            {
+                baseUrl = "https://stackoverflow.com/search?q=" + site;
+            }
             else
             {
                 baseUrl = "https://www.google.com/search?q=" + site;
             }
-            myFunction();
+            GoWeb();
 
             return 0;
         }
         else
         {
             baseUrl = "https://www.google.com/search?q=" + site;
-            myFunction();
+            GoWeb();
         }
     }
+        }
+    
+
+   
 }
-void myFunction()
+void GoWeb()
 {
     system(std::string("start " + baseUrl).c_str());
+}
+void help()
+{
+    int n;
+    std::cout << "## CommandSearch 0.1 ##\nCommands:\nGH - GitHub\nSO - StackOverflow\nLD - LinkedIN\n\nAttention: This is an alpha software, bugs are not welcome, but expected\nType '1' to return back or type '2' to go to the project website.\n\n";
+    std::cin >> n;
+    if (n == 1){main();}
+    else if (n == 2){ProjectWeb = "https://rukavinaet.github.io/CommandSearch";system(std::string("start " + ProjectWeb).c_str());}
+    else{main();}
 }
