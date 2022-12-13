a0262d254557a23354125e560411c6524fec9386
Emanuel Tin Rukavina <rukavinaet@gmail.com>
2022-12-13 21:23:46 +0100

    a

--- CMDSearch.cpp
+++ CMDSearch.cpp
@@ -2,8 +2,8 @@
 #include <cstring>
 using namespace std;
 
-
-//dev branch za .2v
+// dev branch za .2v
+// https://cplusplus.com/forum/beginner/100729/
 
 // globalne stvari
 void GoWeb();
@@ -15,10 +15,11 @@ int main()
 {
     // očisti sustav i pripremi "site"- unos korisnika
     system("cls");
-    std::string site;
-
     std::cout << "## CommandSearch 0.1 ##\nMore information is avaliable at https://rukavinaet.github.io/CommandSearch\nFor help type 'help'\n\n ";
-    std::getline(std::cin, site);
+    std::string first, second;
+    char delimeter('-'); // It is always '-' is it?
+    std::getline(std::cin, first, delimeter);
+    std::getline(std::cin, second);
     if (site == "")
     {
         system("cls");
@@ -36,55 +37,53 @@ int main()
             system("cls");
             help();
         }
-        else{
-             
-        // sve razmake zamijeni sa crticom
-        for (int i = 0; i < site.length(); ++i)
+        else
         {
-            if (site[i] == ' ')
-            {
-                site[i] = '-';
-            }
-        }
 
-        char c = ':';
-        // ovo će se koristit kada bude multiple statements
-        char d = '+';
-        // ako ima dvotočku onda radi ovo
-        if (site.find(c) != std::string::npos)
-        {
-            string first_two = site.substr(0, 2);
-            site.erase(0, 3);
-            if (first_two == "GH")
+            // sve razmake zamijeni sa crticom
+            for (int i = 0; i < site.length(); ++i)
             {
-                baseUrl = "https://github.com/search?q=" + site;
+                if (site[i] == ' ')
+                {
+                    site[i] = '-';
+                }
             }
-            else if (first_two == "LD")
+
+            char c = ':';
+            // ovo će se koristit kada bude multiple statements
+            char d = '+';
+            // ako ima dvotočku onda radi ovo
+            if (site.find(c) != std::string::npos)
             {
-                baseUrl = "https://www.linkedin.com/search/results/all/?keywords=" + site;
-            }
-            else if (first_two == "SO")
-            {
-                baseUrl = "https://stackoverflow.com/search?q=" + site;
+                string first_two = site.substr(0, 2);
+                site.erase(0, 3);
+                if (first_two == "GH")
+                {
+                    baseUrl = "https://github.com/search?q=" + site;
+                }
+                else if (first_two == "LD")
+                {
+                    baseUrl = "https://www.linkedin.com/search/results/all/?keywords=" + site;
+                }
+                else if (first_two == "SO")
+                {
+                    baseUrl = "https://stackoverflow.com/search?q=" + site;
+                }
+                else
+                {
+                    baseUrl = "https://www.google.com/search?q=" + site;
+                }
+                GoWeb();
+
+                return 0;
             }
             else
             {
                 baseUrl = "https://www.google.com/search?q=" + site;
+                GoWeb();
             }
-            GoWeb();
-
-            return 0;
-        }
-        else
-        {
-            baseUrl = "https://www.google.com/search?q=" + site;
-            GoWeb();
         }
     }
-        }
-    
-
-   
 }
 void GoWeb()
 {
@@ -95,7 +94,17 @@ void help()
     int n;
     std::cout << "## CommandSearch 0.1 ##\nCommands:\nGH - GitHub\nSO - StackOverflow\nLD - LinkedIN\n\nAttention: This is an alpha software, bugs are not welcome, but expected\nType '1' to return back or type '2' to go to the project website.\n\n";
     std::cin >> n;
-    if (n == 1){main();}
-    else if (n == 2){ProjectWeb = "https://rukavinaet.github.io/CommandSearch";system(std::string("start " + ProjectWeb).c_str());}
-    else{main();}
+    if (n == 1)
+    {
+        main();
+    }
+    else if (n == 2)
+    {
+        ProjectWeb = "https://rukavinaet.github.io/CommandSearch";
+        system(std::string("start " + ProjectWeb).c_str());
+    }
+    else
+    {
+        main();
+    }
 }
